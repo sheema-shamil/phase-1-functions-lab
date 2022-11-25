401a81ef8b9ca3079a093153b84b4737bb47db03
sheema <sheemashamilfarook@gmail.com>
2022-11-25 12:20:49 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street){
+    return Math.abs(street - 42);
+}
+function distanceFromHqInFeet(distance){
+    return Math.abs(distanceFromHqInBlocks distance)*264;
+}
+function distanceTravelledInFeet(start, destination){
+    return Math.abs((start- destination)*264);
+}
+function calculatesFarePrice(a, b){
+    let distance = distanceTravelledInFeet(a, b)
+    if (distance <= 400){
+        if (distance >= 400 && distance <= 2000)
+    }
+}
+return 2.56;
+if (distance > 2500){
+    return 'cannot travel that far';
+}
+else if(distance >= 2000){
+    return 25;
+}
+footer
+
